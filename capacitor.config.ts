27557b754947a52dc989ba5c72626b5e2063eccd
GDPaduanelli <gennaro.paduanelli@exprivia.com>
2023-04-13 11:03:13 +0200

    no message

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'sfof-mobile-app',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'public',
+  bundledWebRuntime: false,
 };
 
 export default config;
